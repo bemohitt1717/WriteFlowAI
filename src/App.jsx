4d14e122d35943ef0ca4012aafd0a052bdb38739
MOHIT <bemohitt1717@gmail.com>
2026-06-25 14:10:06 +0530

    fixing app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { Analytics } from "@vercel/analytics/react";
 import { AppRoute } from "./routes/AppRoute";
-import { Analytics } from '@vercel/analytics/react';
-
 
 function App() {
-
-  return<> 
-  <AppRoute />
-  <Analytics />
-</>
+  return (
+    <>
+      <AppRoute />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
