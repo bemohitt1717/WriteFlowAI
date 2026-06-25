1026f0056310ea29572fee4c45a3edb82a62f4ed
MOHIT <bemohitt1717@gmail.com>
2026-06-25 17:43:58 +0530

    changing ai model gemini to groq

--- server/Controllers/chatController.js
+++ server/Controllers/chatController.js
@@ -1,4 +1,4 @@
-import { GoogleGenAI } from "@google/genai";
+import { Groq } from "groq-sdk/client.js";
 
 export const chatController = async (req, res) => {
   try {
@@ -11,18 +11,28 @@ export const chatController = async (req, res) => {
       });
     }
 
-    const ai = new GoogleGenAI({
-      apiKey: process.env.GEMINI_API_KEY,
+    const ai = new Groq({
+      apiKey: process.env.GROQ_API_KEY,
     });
 
-    const result = await ai.models.generateContent({
-      model: "gemini-2.5-flash",
-      contents: prompt,
-    });
+    // const result = await ai.models.generateContent({
+    //   model: "gemini-2.5-flash",
+    //   contents: prompt,
+    // });
+
+    const completion = await Groq.Chat.Completions.Create({
+        model: "llama-3.3-70b-versatile",
+  messages: [
+    {
+      role: "user",
+      content: prompt,
+    },
+  ],
+    })
 
     return res.status(200).json({
       success: true,
-      response: result.text || "no response generated",
+      response: completion.choices[0].message.content || "no response generated",
       message: "prompt received successfully",
       prompt,
     });
